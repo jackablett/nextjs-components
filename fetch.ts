483c0f0e2db840f1a716dfc836c45134fa3e21b1
Jack Ablett <80744573+jackablett@users.noreply.github.com>
2025-02-17 07:25:00 +0000

    Update fetch.ts

--- fetch.ts
+++ fetch.ts
@@ -1,25 +1,9 @@
-import { getJWTtoken } from './authorization';
-import type { Product } from '@/models/product/product';
-import { toast } from 'sonner';
-
-export const fetchProduct = async (sku: string): Promise<Product | null> => {
-    return fetchWrapper<Product>(`/Product/${sku}`);
-};
-
 export const fetchWrapper = async <T>(url: string, key?: string | null, method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' = 'GET', formData?: Record<string, unknown>, signal?: AbortController): Promise<T | null> => {
-    const jwtToken = getJWTtoken();
-
-    const baseHeaders: Record<string, unknown> = {
-        "Authorization": `Bearer ${jwtToken || process.env.API_KEY!}`,
-        "SiteId": process.env.SITE_ID!
-    };
-
     return fetch(`${process.env.API_BASE}${url}`, {
         method: method,
         ...formData && { body: JSON.stringify(formData) },
         ...signal && { signal: signal.signal },
         headers: {
-            ...baseHeaders,
             ...formData && { "Content-Type": "application/json" }
         }
     })
@@ -29,8 +13,8 @@ export const fetchWrapper = async <T>(url: string, key?: string | null, method: 
                 return (key ? response.data[key] : response.data) as T;
             }
             else {
-                if (response.message && toast.name) {
-                    toast.error(response.message);
+                if (response.message) {
+                    console.error(response.message)
                 }
                 return null;
             }
@@ -39,15 +23,3 @@ export const fetchWrapper = async <T>(url: string, key?: string | null, method: 
             return null;
         });
 };
-
-export const fetchProductImage = (src: string) => {
-    return `${process.env.MEDIA_BASE}/Images/Productimages/${src}`;
-};
-
-export const fetchImage = (src: string) => {
-    return `${process.env.MEDIA_BASE}/Images/${src}`;
-};
-
-export const fetchCategoryImage = (src: string) => {
-    return `${process.env.MEDIA_BASE}/Images/ProductImages/Thumb/${src}`;
-}
